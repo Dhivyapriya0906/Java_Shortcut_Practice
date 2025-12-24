36ac6ea3d270848e0b289bc9298fc3cde14ba196
Dhivyapriya0906 <indiradhivyapriya@gmail.com>
2025-12-24 19:52:33 +0530

    Bitwise Operator

---
+++ even_odd.java
@@ -0,0 +1,8 @@
+public class even_odd{
+    public static void main(String args[]){
+		int num1=6;
+		int num2=5;
+    System.out.println(((num1 & 1) == 0)+" is even");
+    System.out.println(((num2 & 1) == 1)+" is odd");        //Bitwise Operator use panirukum
+	}
+}
